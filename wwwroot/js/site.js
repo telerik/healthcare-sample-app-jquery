058ded7de49e243a3f76c67ab77591930628c727
Neli Kondova <kondova@progress.com>
2026-04-23 12:28:40 +0300

    add link to the source

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -137,7 +137,70 @@ $(document).ready(function () {
             $("#appbar").removeClass("search-open");
         }
     });
+
+    /* ═══════════════════════════════════════════════
+       GITHUB SOURCE CODE BUTTON
+    ═══════════════════════════════════════════════ */
+    initGithubPopup();
 });
+
+/* ═══════════════════════════════════════════════════════
+   GITHUB SOURCE CODE POPUP
+═══════════════════════════════════════════════════════ */
+function initGithubPopup() {
+    var currentYear = new Date().getFullYear();
+
+    $("#github-popup").kendoWindow({
+        title: false,
+        width: 260,
+        modal: false,
+        visible: false,
+        draggable: false,
+        resizable: false,
+        actions: [],
+        appendTo: ".github-wrap",
+        animation: { open: { duration: 150 }, close: { duration: 100 } },
+        open: function () {
+            this.wrapper.addClass("github-popup");
+            this.wrapper.css({
+                position: "absolute",
+                top: "calc(100% + 8px)",
+                right: 0,
+                left: "auto"
+            });
+        }
+    });
+
+    var win = $("#github-popup").data("kendoWindow");
+    win.content(
+        '<div class="gh-popup-content">' +
+            '<a href="https://github.com/telerik/healthcare-sample-app-jquery" target="_blank" rel="noopener noreferrer" class="gh-link">' +
+                '<svg width="16" height="16" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">' +
+                    '<path fill-rule="evenodd" clip-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>' +
+                '</svg>' +
+                ' Get the Source Code' +
+            '</a>' +
+            '<p class="gh-copyright">Copyright \u00A9 ' + currentYear + ' Progress Software. All rights reserved.</p>' +
+        '</div>'
+    );
+
+    $("#btn-github").on("click", function (e) {
+        e.stopPropagation();
+        var w = $("#github-popup").data("kendoWindow");
+        if (w.wrapper && w.wrapper.is(":visible")) {
+            w.close();
+        } else {
+            w.open();
+        }
+    });
+
+    $(document).on("click", function (e) {
+        if (!$(e.target).closest(".github-wrap").length) {
+            var w = $("#github-popup").data("kendoWindow");
+            if (w) w.close();
+        }
+    });
+}
 
 /* ═══════════════════════════════════════════════════════
    GLOBAL PATIENT HEADER SEARCH
